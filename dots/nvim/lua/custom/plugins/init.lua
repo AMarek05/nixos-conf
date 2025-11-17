f0406d560d0f88434496ae25d738afbaef68dcd0
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-18 00:00:32 +0100

    Daily commit: 2025-11-18

---
+++ dots/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,42 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'nvim-neorg/neorg',
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = '*', -- Pin Neorg to the latest stable release
+    config = function()
+      require('neorg').setup {
+        load = {
+          ['core.defaults'] = {},
+          ['core.concealer'] = {},
+          ['core.dirman'] = {
+            config = {
+              workspaces = {
+                notes = '~/gdrive/Notes/neorg/',
+              },
+              default_workspace = 'notes',
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    'aserowy/tmux.nvim',
+    lazy = false,
+    version = '*',
+    config = function()
+      require('tmux').setup {
+        copy_sync = {
+          redirect_to_clipboard = true,
+        },
+      }
+    end,
+  },
+  {
+    'farmergreg/vim-lastplace',
+  },
+}
