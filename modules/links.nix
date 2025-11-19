91b6a2026cfb25b11ac93bb0f0f179a790861cc0
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-19 20:21:36 +0100

    20:21 - 19 November 2025

--- modules/links.nix
+++ modules/links.nix
@@ -1,5 +1,7 @@
 { config, lib, ... }:
-
+let
+  dotsPath = ../store;
+in
 {
   options.modules.links = {
     enable = lib.mkEnableOption "links";
@@ -7,10 +9,10 @@
 
   config = lib.mkIf config.modules.links.enable {
     home.file = {
-      ".config/starship.toml".source = dots/starship/starship.toml;
-      ".config/.transient_prompt".source = dots/starship/.transient_prompt;
-      ".config/nvim".source = dots/nvim;
-      "Scripts".source = dots/Scripts;
+      ".config/starship.toml".source = /. + dotsPath + "/starship/starship.toml";
+      ".config/.transient_prompt".source = /. + dotsPath + "/starship/.transient_prompt";
+      ".config/nvim".source = /. + dotsPath + "/nvim";
+      "Scripts".source = /. + dotsPath + "/Scripts";
     };
   };
 }
