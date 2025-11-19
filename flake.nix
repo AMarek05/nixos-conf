4eed1e58dc4f2e2bd055655b8bd5f5a2c6f6b3b0
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-19 19:28:52 +0100

    19:28 - 19 November 2025

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
         pkgs = nixpkgs.legacyPackages."x86_64-linux";
 
         modules = [
-          ./home.nix
+          ./hosts/nixos.nix
         ];
 
         extraSpecialArgs = {
---
+++ hosts/nixos.nix
@@ -0,0 +1,7 @@
+{ ... }:
+{
+  imports = [
+    ../modules/defaults.nix
+    ./common.nix
+  ];
+}
