86f09a227fe306a5c4e699482c2a14efa79cacdf
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-20 23:11:10 +0100

    23:11 - 20 listopada 2025

---
+++ modules/hyprland.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    systemd.enable = false;
+
+    settings = {
+      exec-once = [
+        "uwsm finalize"
+      ];
+
+      bind = [
+        "$mainMod, A, exec, uwsm app -- rofi -show drun"
+      ];
+    };
+  };
+  programs.waybar.systemd.enable = true;
+  services.dunst.enable = true;
+  services.sww.enable = true;
+
+  programs.rofi = {
+    enable = true;
+    package = pkgs.rofi-wayland;
+
+    extraConfig = {
+      modi = "drun,run";
+      show-icons = true;
+      drun-display-format = "{icon} {name}";
+      disable-history = false;
+      hide-scrollbar = true;
+      sudebar-mode = true;
+    };
+  };
+}
