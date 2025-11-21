4cfdc0e7355e1289fd62ee020dc8bdc15519671e
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-21 15:49:28 +0100

    15:49 - 21 listopada 2025

--- store/nvim/lua/custom/plugins/init.lua
+++ store/nvim/lua/custom/plugins/init.lua
@@ -3,27 +3,27 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'nvim-neorg/neorg',
-    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
-    version = '*', -- Pin Neorg to the latest stable release
-    config = function()
-      require('neorg').setup {
-        load = {
-          ['core.defaults'] = {},
-          ['core.concealer'] = {},
-          ['core.dirman'] = {
-            config = {
-              workspaces = {
-                notes = '~/gdrive/Notes/neorg/',
-              },
-              default_workspace = 'notes',
-            },
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'nvim-neorg/neorg',
+  --   lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  --   version = '*', -- Pin Neorg to the latest stable release
+  --   config = function()
+  --     require('neorg').setup {
+  --       load = {
+  --         ['core.defaults'] = {},
+  --         ['core.concealer'] = {},
+  --         ['core.dirman'] = {
+  --           config = {
+  --             workspaces = {
+  --               notes = '~/gdrive/Notes/neorg/',
+  --             },
+  --             default_workspace = 'notes',
+  --           },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     'aserowy/tmux.nvim',
     lazy = false,
