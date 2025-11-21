7acf7b9908b9dc97f6437db764c004db7cc3e668
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-21 14:30:58 +0100

    14:30 - 21 listopada 2025

--- modules/apps/terminal.nix
+++ modules/apps/terminal.nix
@@ -7,8 +7,21 @@
       terminal = "xterm-256color";
       shell = "${pkgs.zsh}/bin/zsh";
 
+      sensibleOnTop = true;
+
       prefix = "C-a";
       newSession = true;
+
+      plugins = [
+        {
+          plugin = pkgs.tmuxPlugins.tokyo-night-tmux;
+          extraConfig = ''
+            set -g @tokyo-night-tmux_date_format DMY
+            set -g @tokyo-night-tmux_time_format 24H
+            set -g @tokyo-night-tmux_show_battery_widget 0
+          '';
+        }
+      ];
     };
 
     ghostty = {
