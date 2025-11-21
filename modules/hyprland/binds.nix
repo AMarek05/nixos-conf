85489b802cde83cd3e2dfb47beaea0fc7056f2a0
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-21 11:55:46 +0100

    setup

--- modules/hyprland/binds.nix
+++ modules/hyprland/binds.nix
@@ -2,49 +2,50 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
+      "$mod" = "Super";
       bind = [
-        "Super, Escape, exit,"
-        "Super, Q, killactive,"
+        "$mod, Escape, exit,"
+        "$mod, Q, killactive,"
 
         # apps
-        "Super Control, Return, exec, uwsm app -- rofi -show drun"
-        "Super, Return, exec, uwsm app -- ghostty"
+        "$mod Control, Return, exec, uwsm app -- rofi -show drun"
+        "$mod, Return, exec, uwsm app -- ghostty"
 
         # move focus
-        "Super, H, movefocus, l"
-        "Super, J, movefocus, d"
-        "Super, K, movefocus, u"
-        "Super, L, movefocus, r"
+        "$mod, H, movefocus, l"
+        "$mod, J, movefocus, d"
+        "$mod, K, movefocus, u"
+        "$mod, L, movefocus, r"
 
         # swtich workspace
-        "Super, 1, workspace, 1"
-        "Super, 2, workspace, 2"
-        "Super, 3, workspace, 3"
-        "Super, 4, workspace, 4"
-        "Super, 5, workspace, 5"
-        "Super, 6, workspace, 6"
-        "Super, 7, workspace, 7"
-        "Super, 8, workspace, 8"
-        "Super, 9, workspace, 9"
-        "Super, 0, workspace, 10"
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+        "$mod, 0, workspace, 10"
 
         # move window to workspace
-        "Super Shift, 1, movetoworkspace, 1"
-        "Super Shift, 2, movetoworkspace, 2"
-        "Super Shift, 3, movetoworkspace, 3"
-        "Super Shift, 4, movetoworkspace, 4"
-        "Super Shift, 5, movetoworkspace, 5"
-        "Super Shift, 6, movetoworkspace, 6"
-        "Super Shift, 7, movetoworkspace, 7"
-        "Super Shift, 8, movetoworkspace, 8"
-        "Super Shift, 9, movetoworkspace, 9"
-        "Super Shift, 0, movetoworkspace, 10"
+        "$mod Shift, 1, movetoworkspace, 1"
+        "$mod Shift, 2, movetoworkspace, 2"
+        "$mod Shift, 3, movetoworkspace, 3"
+        "$mod Shift, 4, movetoworkspace, 4"
+        "$mod Shift, 5, movetoworkspace, 5"
+        "$mod Shift, 6, movetoworkspace, 6"
+        "$mod Shift, 7, movetoworkspace, 7"
+        "$mod Shift, 8, movetoworkspace, 8"
+        "$mod Shift, 9, movetoworkspace, 9"
+        "$mod Shift, 0, movetoworkspace, 10"
       ];
 
       # mouse binds
       bindm = [
-        "Super, mouse:272, movewindow"
-        "Super, mouse:273, resizewindow"
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
       ];
 
       bindel = [
