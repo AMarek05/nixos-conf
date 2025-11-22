6e3ef598484170c5f769b9f1892ff8ad944895b0
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-22 13:34:30 +0100

    13:34 - 22 listopada 2025

---
+++ modules/apps/nvim/opts.nix
@@ -0,0 +1,41 @@
+{ ... }:
+{
+  programs.nvf.settings.vim.options = {
+    options = {
+      expandtab = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      softtabstop = 2;
+
+      number = true;
+      relativenumber = true;
+
+      mouse = "a";
+      showmode = false;
+
+      clipboard = "unnamedplus";
+      breakindent = true;
+      undofile = true;
+
+      ignorecase = true;
+      smartcase = true;
+
+      signcolumn = "yes";
+
+      updatetime = 250;
+      timeoutlen = 300;
+
+      splitright = true;
+      splitbelow = true;
+
+      list = true;
+      listchars = "tab:» ,trail:·,nbsp:␣";
+
+      inccommand = "split";
+      cursorline = true;
+      scrolloff = 5;
+
+      hlsearch = true;
+    };
+  };
+}
