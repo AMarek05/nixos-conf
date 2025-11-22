0b286b940c4395795800dec6f4952679876206b7
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-22 13:29:00 +0100

    13:29 - 22 listopada 2025

--- modules/apps/nvf.nix
+++ modules/apps/nvf.nix
@@ -8,8 +8,8 @@
   programs.nvf = {
     enable = true;
 
-    settings = {
-      vim.options = {
+    settings.vim = {
+      options = {
         expandtab = true;
         tabstop = 2;
         shiftwidth = 2;
@@ -45,6 +45,91 @@
 
         hlsearch = true;
       };
+      keymaps = [
+        {
+          key = "<Esc>";
+          mode = [ "n" ];
+          action = "<cmd>nohlsearch<CR>";
+        }
+        {
+          key = "<leader>e";
+          mode = [ "n" ];
+          action = "vim.diagnosctic.open_float";
+          desc = "Show diagnostic [Error] messages";
+        }
+        {
+          key = "<leader>q";
+          mode = [ "n" ];
+          action = "vim.diagnostic.setloclist";
+          desc = "Open diagnostic [Q]uickfix list";
+        }
+        {
+          key = "<C-h>";
+          mode = [ "n" ];
+          action = "<C-w><C-h>";
+          desc = "Move focus to the left window";
+        }
+        {
+          key = "<C-j>";
+          mode = [ "n" ];
+          action = "<C-w><C-j>";
+          desc = "Move focus to the lower window";
+        }
+        {
+          key = "<C-k>";
+          mode = [ "n" ];
+          action = "<C-w><C-k>";
+          desc = "Move focus to the upper window";
+        }
+        {
+          key = "<C-l>";
+          mode = [ "n" ];
+          action = "<C-w><C-l>";
+          desc = "Move focus to the right window";
+        }
+        {
+          key = "<C-w><";
+          mode = [ "n" ];
+          action = "<cmd>vertical resize -5<CR>";
+          desc = "Decrease width";
+        }
+        {
+          key = "<C-w>>";
+          mode = [ "n" ];
+          action = "<cmd>vertical resize +5<CR>";
+          desc = "Increase width";
+        }
+        {
+          key = "<C-w>-";
+          mode = [ "n" ];
+          action = "<cmd>resize +5<CR>";
+          desc = "Increase width";
+        }
+        {
+          key = "<C-w>+";
+          mode = [ "n" ];
+          action = "<cmd>resize +5<CR>";
+          desc = "Increase width";
+        }
+        {
+          key = "<C-n>";
+          mode = [ "n" ];
+          action = "<cmd>tabn<CR>";
+          desc = "Go to the next tab";
+        }
+        {
+          key = "<C-p>";
+          mode = [ "n" ];
+          action = "<cmd>tabp<CR>";
+          desc = "Go to the previous tab";
+        }
+        {
+          key = "<leader>nt";
+          mode = [ "n" ];
+          action = "<cmd>tabnew<CR>";
+          desc = "[N]ew [T]ab";
+        }
+      ];
     };
   };
 }
