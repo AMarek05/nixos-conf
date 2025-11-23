3578d5c4a6e878841f887296da708fe3ed871513
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-23 15:14:02 +0100

    15:14 - 23 listopada 2025

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -163,6 +163,8 @@
     stdenv.cc.cc.lib
   ];
 
+  programs.direnv.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
