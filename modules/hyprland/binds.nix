5c5a1cd485b292c75903999d323cc4610f3b5bd0
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-11-25 16:27:26 +0100

    16:27 - 25 listopada 2025

--- modules/hyprland/binds.nix
+++ modules/hyprland/binds.nix
@@ -12,7 +12,7 @@
         "$mod, Space, togglefloating,"
 
         "Alt, Tab, cyclenext,"
-        "Alt Shift, Tab, cycleprevious,"
+        "Alt Shift, Tab, cycle, prev"
 
         "Alt, Tab, bringactivetotop,"
         "Alt Shift, Tab, bringactivetotop,"
