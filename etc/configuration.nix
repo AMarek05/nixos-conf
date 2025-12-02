2ab34953f847041a64de10dff43ca5ec9a65b53e
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-12-02 17:27:32 +0100

    17:27 - 02 grudnia 2025

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -174,6 +174,8 @@
     enableZshIntegration = true;
   };
 
+  services.flatpak.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   programs.mtr.enable = true;
