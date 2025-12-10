35b24217317e70f2b994d387cb58d9c2848ec208
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-12-10 17:48:07 +0100

    17:48 - 10 grudnia 2025

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -1,4 +1,9 @@
-{ inputs, lib, config, ... }:
+{
+  inputs,
+  lib,
+  config,
+  ...
+}:
 let
   cfg = config.modules.hyprland;
 in
@@ -17,90 +22,89 @@ in
   ];
 
   config = lib.mkIf cfg.enable {
-
     wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = false;
+      enable = true;
+      systemd.enable = false;
 
-    settings = {
-      exec-once = [
-        "uwsm app -- ashell"
-      ];
-      input = {
-        kb_layout = "pl,us";
-      };
-      general = {
-        gaps_in = 10;
-        gaps_out = 15;
-
-        border_size = 2;
-      };
-
-      dwindle = {
-        preserve_split = true;
-        smart_split = false;
-        smart_resizing = false;
-      };
-    };
-
-  };
-
-  programs.ashell = {
-    enable = true;
-
-    settings = {
-      modules = {
-        left = [ "WindowTitle" ];
-        center = [ "Workspaces" ];
-        right = [
-          "SystemInfo"
-          [
-            "Clock"
-            "Privacy"
-          ]
-          "Settings"
+      settings = {
+        exec-once = [
+          "uwsm app -- ashell"
         ];
+        input = {
+          kb_layout = "pl,us";
+        };
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
+
+          border_size = 2;
+        };
+
+        dwindle = {
+          preserve_split = true;
+          smart_split = false;
+          smart_resizing = false;
+        };
       };
 
-      workspaces.enable_workspace_filling = true;
-      workspaces.max_workspaces = 5;
+    };
 
-      window_title.mode = "Title";
-      window_title.truncate_title_after_length = 50;
+    programs.ashell = {
+      enable = true;
 
-      appearance = {
-        font_name = "JetBrainsMono Nerd Font Propo";
-        scale_factor = 1.20;
+      settings = {
+        modules = {
+          left = [ "WindowTitle" ];
+          center = [ "Workspaces" ];
+          right = [
+            "SystemInfo"
+            [
+              "Clock"
+              "Privacy"
+            ]
+            "Settings"
+          ];
+        };
+
+        workspaces.enable_workspace_filling = true;
+        workspaces.max_workspaces = 5;
+
+        window_title.mode = "Title";
+        window_title.truncate_title_after_length = 50;
+
+        appearance = {
+          font_name = "JetBrainsMono Nerd Font Propo";
+          scale_factor = 1.20;
+        };
       };
     };
-  };
 
-  programs.walker = {
-    enable = true;
-    runAsService = true;
+    programs.walker = {
+      enable = true;
+      runAsService = true;
 
-    config = {
-      app_launch_prefix = "uwsm app -- ";
-      terminal = "ghostty";
+      config = {
+        app_launch_prefix = "uwsm app -- ";
+        terminal = "ghostty";
 
-      disable_mouse = true;
-      close_when_open = true;
+        disable_mouse = true;
+        close_when_open = true;
 
-      search.placeholder = "Search...";
+        search.placeholder = "Search...";
+      };
     };
-  };
 
-  programs.rofi = {
-    enable = true;
+    programs.rofi = {
+      enable = true;
 
-    extraConfig = {
-      modi = "drun,run";
-      show-icons = true;
-      drun-display-format = "{icon} {name}";
-      disable-history = false;
-      hide-scrollbar = true;
-      sidebar-mode = true;
+      extraConfig = {
+        modi = "drun,run";
+        show-icons = true;
+        drun-display-format = "{icon} {name}";
+        disable-history = false;
+        hide-scrollbar = true;
+        sidebar-mode = true;
+      };
     };
-  };
   };
 }
