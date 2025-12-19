29deb14b6bf1ced59ba6d18e4da375fd4c4ee995
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2025-12-19 23:46:58 +0100

    23:46 - 19 grudnia 2025

--- etc/nvidia.nix
+++ etc/nvidia.nix
@@ -1,9 +1,20 @@
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  boot.tmp.useTmpfs = lib.mkForce false;
+
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
+
+    powerManagement.enable = true;
+    powerManagement.finegrained = false;
 
     open = true;
     nvidiaSettings = true;
@@ -13,9 +24,14 @@
 
   hardware.graphics.extraPackages = with pkgs; [
     nvidia-vaapi-driver
-
     libva-utils
-
     libva-vdpau-driver
+  ];
+
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 12 * 1024;
+    }
   ];
 }
