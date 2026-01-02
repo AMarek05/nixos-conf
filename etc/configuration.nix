cafa4a0ae280c42c84bee94b5888637d9e35a574
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2026-01-02 23:07:05 +0100

    23:07 - 02 stycznia 2026

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -246,7 +246,7 @@
   };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
