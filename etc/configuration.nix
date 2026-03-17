c00a8d3023227531024f72006d80a7fe25e3655e
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2026-03-17 22:55:30 +0100

    Add paralelism

--- etc/configuration.nix
+++ etc/configuration.nix
@@ -25,6 +25,9 @@
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
+
+    max-jobs = "auto";
+    cores = 0;
   };
 
   nix.settings.trusted-users = [
