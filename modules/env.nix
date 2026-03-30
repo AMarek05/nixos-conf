1a8e0b61d3aba03deec9ad50538e625212e7829d
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2026-03-30 15:56:32 +0200

    Migrate scripts to module

--- modules/env.nix
+++ modules/env.nix
@@ -12,7 +12,6 @@
   config = lib.mkIf config.modules.env.enable {
     home.sessionVariables = {
       SHELL = pkgs.zsh;
-      PATH = "$PATH:/home/adam/Scripts:/home/adam/.cargo/bin";
       NH_FLAKE = "/home/adam/sys";
       EDITOR = "nvim";
       VISUAL = "nvim";
