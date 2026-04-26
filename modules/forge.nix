818ae8f980f5942cd1116f10477ac232056172a7
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2026-04-26 22:17:06 +0200

    Fix incorrect zsh field

--- modules/forge.nix
+++ modules/forge.nix
@@ -30,7 +30,7 @@
   # Completions are installed by the module via home.file
   # ("share/zsh/site-functions/_forge")
   # Use prependZshrc so fpath is updated BEFORE compinit runs
-  programs.zsh.prependZshrc = ''
+  programs.zsh.initExtraBeforeCompInit = ''
     fpath=("${config.forge.package}/share/zsh/site-functions" $fpath)
   '';
 }
