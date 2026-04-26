a7d68025fae2a272fe3c66b9a687bba46fa6eb8d
Claw <278452676+amarek-machine@users.noreply.github.com>
2026-04-26 19:38:18 +0200

    nvim: override gitsigns toggleBlame/toggleDeleted to gtb/gtd under git group

--- modules/apps/nvim/plugins/git.nix
+++ modules/apps/nvim/plugins/git.nix
@@ -1,12 +1,15 @@
-{
-  pkgs,
-  ...
-}:
+{ pkgs, lib, ... }:
 {
   programs.nvf.settings.vim = {
     git = {
       enable = true;
       neogit.enable = true;
+      gitsigns = {
+        mappings = {
+          toggleBlame = lib.nvim.binds.mkKeymap "<leader>gtb";
+          toggleDeleted = lib.nvim.binds.mkKeymap "<leader>gtd";
+        };
+      };
       gitsigns.setupOpts = {
         signs = {
           add = {
