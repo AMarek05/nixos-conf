ef231a756339fe71ba5951774bbc7045f77a1d1d
Adam Marek <118975111+AMarek05@users.noreply.github.com>
2026-04-27 01:54:55 +0200

    Change back insertion

--- modules/forge.nix
+++ modules/forge.nix
@@ -30,7 +30,7 @@
   # Completions are installed by the module via home.file
   # ("share/zsh/site-functions/_forge")
   # Use prependZshrc so fpath is updated BEFORE compinit runs
-  programs.zsh.initExtraBeforeCompInit = ''
+  programs.zsh.initContent = ''
     fpath=("${config.forge.package}/share/zsh/site-functions" $fpath)
   '';
 }
