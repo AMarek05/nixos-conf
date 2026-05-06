973942b2d51a174d0426a593d77dbbefe4e72c4e
Adam Marek <amarek.git@gmail.com>
2026-05-06 19:25:47 +0200

    flake: remove lix input, refactor lix
    - remove the lix module from the flake inputs
    - move the pkgs.lix substitution into a common.nix hm.module
    - shift laptop-specific options to the appropriate host file
    - clean the flake file up

--- hosts/nixos-laptop.nix
+++ hosts/nixos-laptop.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [
     ./common.nix
   ];
+
+  programs.zsh.shellAliases = {
+    nhh = lib.mkForce "nh home switch --cores 4 --max-jobs 1";
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = lib.mkForce [ ", 1920x1080@59.997000, auto, 1" ];
+    input.touchpad = {
+      natural_scroll = true;
+      scroll_factor = 0.3;
+    };
+  };
 }
