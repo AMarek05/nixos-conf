dd05b292e1fdbaf81e12f5287eab3ccfecd1448c
Adam Marek <amarek.git@gmail.com>
2026-05-08 20:04:28 +0200

    caelestia: change default timeout, disable on PC

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./common.nix
+  ];
+
+  programs.caelestia.settings.general.idle.timeouts = lib.mkForce [
+    {
+      timeout = 600;
+      idleAction = "dpms off";
+      returnAction = "dpms on";
+    }
   ];
 
   home.packages = with pkgs; [
