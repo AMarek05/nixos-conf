55a05c88ff59bcfc9ff5ccff205f205e7c8c5d6c
Adam Marek <amarek.git@gmail.com>
2026-05-18 15:38:11 +0200

    nvidia: change vars add kernel param

--- etc/nvidia.nix
+++ etc/nvidia.nix
@@ -5,16 +5,19 @@
   ...
 }:
 {
-  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+  boot.kernelParams = [
+    "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    "nvidia-drm.modeset=1"
+  ];
+
   boot.tmp.useTmpfs = lib.mkForce false;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
   environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "nvidia";
+    LIBVA_DRIVE_NAME = "nvidia";
     XDG_SESSION_TYPE = "wayland";
     GBM_BACKEND = "nvidia-drm";
-    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     # Forces Mozilla to use the Wayland EGL backend properly
     MOZ_ENABLE_WAYLAND = "1";
   };
