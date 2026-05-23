be15c277b69b27c73fa59ffe36ade86203143d44
Claw <278452676+amarek-machine@users.noreply.github.com>
2026-05-23 22:40:26 +0200

    Revert "refactor lib/modules.nix: optional → disabledByDefault, omit when default"
    This reverts commit 51c897e17276e9a30b3b090e4b2c0fbb6b4f3ae3.

--- modules/default.nix
+++ modules/default.nix
@@ -7,43 +7,92 @@ modulesLib.mkHostHmModules {
   basePath = ../modules;
   entries = [
     # files
-    { name = "env";    kind = "file"; }
-    { name = "git";    kind = "file"; }
-    { name = "links";  kind = "file"; }
-    { name = "util";   kind = "file"; }
+    {
+      name = "env";
+      kind = "file";
+      optional = false;
+    }
+    {
+      name = "git";
+      kind = "file";
+      optional = false;
+    }
+    {
+      name = "links";
+      kind = "file";
+      optional = false;
+    }
+    {
+      name = "util";
+      kind = "file";
+      optional = false;
+    }
     # dirs with nested options
     {
       name = "apps";
       kind = "dir";
+      optional = false;
       sub = [
-        { name = "stylix"; }
-        { name = "nvf";    }
+        {
+          name = "stylix";
+          optional = false;
+        }
+        {
+          name = "nvf";
+          optional = false;
+        }
       ];
     }
-    { name = "caelestia"; kind = "dir"; }
+    {
+      name = "caelestia";
+      kind = "dir";
+      optional = false;
+    }
     {
       name = "hyprland";
       kind = "dir";
+      optional = false;
       sub = [
-        { name = "caelestia"; }
+        {
+          name = "caelestia";
+          optional = false;
+        }
       ];
     }
     {
       name = "shell";
       kind = "dir";
+      optional = false;
       sub = [
-        { name = "zsh";       }
-        { name = "starship"; disabledByDefault = true; }
+        {
+          name = "zsh";
+          optional = false;
+        }
+        {
+          name = "starship";
+          optional = true;
+        }
       ];
     }
     {
       name = "terminal";
       kind = "dir";
+      optional = false;
       sub = [
-        { name = "ghostty"; }
-        { name = "man";     }
-        { name = "tmux";    }
+        {
+          name = "ghostty";
+          optional = false;
+        }
+        {
+          name = "man";
+          optional = false;
+        }
+        {
+          name = "tmux";
+          optional = false;
+        }
       ];
     }
   ];
 }
+
