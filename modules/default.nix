ddb99e0edd8e3e98309ab734b4426e109d49fd7d
Claw <278452676+amarek-machine@users.noreply.github.com>
2026-05-23 22:42:56 +0200

    clean up default.nix entries: omit optional = false, only keep optional = true

--- modules/default.nix
+++ modules/default.nix
@@ -7,92 +7,43 @@ modulesLib.mkHostHmModules {
   basePath = ../modules;
   entries = [
     # files
-    {
-      name = "env";
-      kind = "file";
-      optional = false;
-    }
-    {
-      name = "git";
-      kind = "file";
-      optional = false;
-    }
-    {
-      name = "links";
-      kind = "file";
-      optional = false;
-    }
-    {
-      name = "util";
-      kind = "file";
-      optional = false;
-    }
+    { name = "env";   kind = "file"; }
+    { name = "git";   kind = "file"; }
+    { name = "links"; kind = "file"; }
+    { name = "util";  kind = "file"; }
     # dirs with nested options
     {
       name = "apps";
       kind = "dir";
-      optional = false;
       sub = [
-        {
-          name = "stylix";
-          optional = false;
-        }
-        {
-          name = "nvf";
-          optional = false;
-        }
+        { name = "stylix"; }
+        { name = "nvf";    }
       ];
     }
-    {
-      name = "caelestia";
-      kind = "dir";
-      optional = false;
-    }
+    { name = "caelestia"; kind = "dir"; }
     {
       name = "hyprland";
       kind = "dir";
-      optional = false;
       sub = [
-        {
-          name = "caelestia";
-          optional = false;
-        }
+        { name = "caelestia"; }
       ];
     }
     {
       name = "shell";
       kind = "dir";
-      optional = false;
       sub = [
-        {
-          name = "zsh";
-          optional = false;
-        }
-        {
-          name = "starship";
-          optional = true;
-        }
+        { name = "zsh";       }
+        { name = "starship"; optional = true; }
       ];
     }
     {
       name = "terminal";
       kind = "dir";
-      optional = false;
       sub = [
-        {
-          name = "ghostty";
-          optional = false;
-        }
-        {
-          name = "man";
-          optional = false;
-        }
-        {
-          name = "tmux";
-          optional = false;
-        }
+        { name = "ghostty"; }
+        { name = "man";     }
+        { name = "tmux";    }
       ];
     }
   ];
 }
-
