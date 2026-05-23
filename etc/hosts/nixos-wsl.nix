d127552e04ec6555ed98c4c974aa2266ae27c863
Claw <278452676+amarek-machine@users.noreply.github.com>
2026-05-24 00:43:16 +0200

    fix: remove stale openclaw.nix and nixos-wsl modules block
    etc/hosts/nixos.nix: removed ../openclaw.nix import (now via modules/nixos/)
    and removed modules.sandbox.enable (nixos module option path no longer applies)
    
    etc/hosts/nixos-wsl.nix: removed modules = { ... } block (old way of disabling
    modules, now handled by not importing them in the first place)

--- etc/hosts/nixos-wsl.nix
+++ etc/hosts/nixos-wsl.nix
@@ -13,19 +13,4 @@
 
   boot.loader.grub.enable = lib.mkForce false;
   boot.loader.systemd-boot.enable = lib.mkForce false;
-
-  modules = {
-    audio.enable = lib.mkForce false;
-    console.enable = lib.mkForce false;
-    fonts.enable = lib.mkForce false;
-    gamemode.enable = lib.mkForce false;
-    networking.enable = lib.mkForce false;
-    packages.enable = lib.mkForce false;
-    security.enable = lib.mkForce false;
-    shell.enable = lib.mkForce false;
-    user.enable = lib.mkForce false;
-    nix-ld.enable = lib.mkForce false;
-    sandbox.enable = lib.mkForce false;
-    vpn.enable = lib.mkForce false;
-  };
 }
