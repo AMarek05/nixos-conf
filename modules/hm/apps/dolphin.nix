ecbd28a5b7a3c96a9a4598383138893b3c5273cc
Adam Marek <amarek.git@gmail.com>
2026-05-24 17:52:09 +0200

    apps: guard dolphin with mkIf

--- modules/hm/apps/dolphin.nix
+++ modules/hm/apps/dolphin.nix
@@ -1,18 +1,27 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    (symlinkJoin {
-      name = "dolphin-xcb";
-      paths = [ kdePackages.dolphin ]; # Use pkgs.dolphin if on older KDE 5
-      nativeBuildInputs = [ makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/dolphin \
-          --set QT_QPA_PLATFORM xcb
-      '';
-    })
-    kdePackages.dolphin-plugins
-    kdePackages.kio
-    kdePackages.kio-extras
-    kdePackages.ffmpegthumbs
-  ];
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  options.hmModules.apps.dolphin.enable = lib.mkEnableOption "Enable the dolphin file explorer";
+
+  config = lib.mkIf config.hmModules.apps.dolphin.enable {
+    home.packages = with pkgs; [
+      (symlinkJoin {
+        name = "dolphin-xcb";
+        paths = [ kdePackages.dolphin ];
+        nativeBuildInputs = [ makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/dolphin \
+            --set QT_QPA_PLATFORM xcb
+        '';
+      })
+      kdePackages.dolphin-plugins
+      kdePackages.kio
+      kdePackages.kio-extras
+      kdePackages.ffmpegthumbs
+    ];
+  };
 }
