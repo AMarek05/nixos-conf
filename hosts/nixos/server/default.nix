c6ba9afc773ce5f256bf3358d6736da7f49f7445
Adam Marek <amarek.git@gmail.com>
2026-05-27 22:42:37 +0200

    security: add cloudflare api auth

--- hosts/nixos/server/default.nix
+++ hosts/nixos/server/default.nix
@@ -1,6 +1,33 @@
-{ pkgs, lib, ... }:
+{ config, lib, ... }:
 {
-  networking.firewall.enable = false;
+
+  sops.secrets."cloudflare_dns_key" = {
+    sopsFile = ../../../secrets/serv.yaml;
+    owner = "acme";
+    group = "acme";
+    mode = 0400;
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "amarek05@pm.me";
+
+    certs."amarek.org" = {
+      domain = "*.amarek.org";
+      extraDomainNames = [ "amarek.org" ];
+      dnsProvider = "cloudflare";
+
+      credentialFiles = {
+        "CLOUDFLARE_DNS_API_TOKEN_FILE" = config.sops.secrets."cloudflare_dns_key".path;
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    22
+    80
+    443
+  ];
 
   services.sillytavern = {
     enable = true;
@@ -14,9 +41,14 @@
     virtualHosts."st.amarek.org".extraConfig = ''
       reverse_proxy localhost:8000
     '';
+    virtualHosts."plex.amarek.org".extraConfig = ''
+      reverse_proxy localhost:32400
+    '';
   };
 
   services.plex = {
     enable = true;
+
+    openFirewall = true;
   };
 }
