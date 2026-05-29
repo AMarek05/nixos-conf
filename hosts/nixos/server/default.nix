21fad530d41e97a06a3112b161af53123a55869b
Claw <278452676+amarek-machine@users.noreply.github.com>
2026-05-29 15:48:14 +0200

    openclaw: remove inputs from container config (sops-nix not accessible inside eval context)

--- hosts/nixos/server/default.nix
+++ hosts/nixos/server/default.nix
@@ -11,7 +11,7 @@
     hostAddress = "192.168.100.10";
     localAddress = "192.168.100.11";
 
-    config = { config, pkgs, inputs, ... }: {
+    config = { config, pkgs, ... }: {
       imports = [ ./openclaw.nix ];
     };
 
