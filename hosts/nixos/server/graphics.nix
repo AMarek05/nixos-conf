c23a6475c7ca002db17b5175e733eb086b5fd2e5
Adam Marek <amarek.git@gmail.com>
2026-05-30 22:24:09 +0200

    fix: remove reference to plex user

--- hosts/nixos/server/graphics.nix
+++ hosts/nixos/server/graphics.nix
@@ -5,17 +5,11 @@
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
-      intel-media-driver # For Broadwell (5th Gen) or newer (LIBVA_DRIVER_NAME=iHD)
-      intel-vaapi-driver # For older processors (LIBVA_DRIVER_NAME=i965)
+      intel-media-driver
+      intel-vaapi-driver
       libvdpau-va-gl
     ];
   };
-
-  # Ensure the Plex user has access to the render node
-  users.users.plex.extraGroups = [
-    "render"
-    "video"
-  ];
 
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "iHD";
