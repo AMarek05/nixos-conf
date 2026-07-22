c309beb7df44b40bea5e6de77bdaa800b30954f9
Adam Marek <amarek.git@gmail.com>
2026-07-23 00:30:16 +0200

    laptop: migrate overrides

--- hosts/hm/nixos-laptop.nix
+++ hosts/hm/nixos-laptop.nix
@@ -8,9 +8,17 @@
     nhh = lib.mkForce "nh home switch --cores 4 --max-jobs 1";
   };
 
-  wayland.windowManager.hyprland.settings = {
-    monitor = lib.mkForce [ ", 1920x1080@59.997000, auto, 1" ];
-    input.touchpad = {
+  hmModules.hyprland = {
+    monitors = lib.mkForce [
+      {
+        output = "";
+        mode = "1920x1080@59.997000";
+        position = "auto";
+        scale = 1;
+      }
+    ];
+
+    settings.input.touchpad = {
       natural_scroll = true;
       scroll_factor = 0.3;
     };
