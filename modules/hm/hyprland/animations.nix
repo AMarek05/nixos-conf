454ef43aabfe29cca827041b2d47757a93c1a838
Adam Marek <amarek.git@gmail.com>
2026-07-22 23:32:13 +0200

    hyprland: migrate animations.nix

--- modules/hm/hyprland/animations.nix
+++ modules/hm/hyprland/animations.nix
@@ -1,23 +1,86 @@
-{ ... }:
 {
-  wayland.windowManager.hyprland.settings.animations = {
-    enabled = true;
+  lib,
+  config,
+  myLib,
+  ...
+}:
+let
+  # import lua parser
+  inherit (myLib) toLua;
 
-    # Bezier curves must be a list of comma-separated strings
-    # Format: NAME, X0, Y0, X1, Y1
-    bezier = [
-      "easeInOut, 0.4, 0, 0.2, 1"
-    ];
+  cfg = config.hmModules.hyprland.animations;
 
-    # Animations must be a list of comma-separated strings
-    # Format: NAME, ONOFF, SPEED, CURVE
-    # Note: SPEED is in 100ms units. (e.g., 1.5 = 150ms, 2 = 200ms)
-    animation = [
-      "workspaces, 1, 1.5, easeInOut"
-      "windows, 1, 2, easeInOut"
-      "windowsIn, 1, 1.5, easeInOut" # Hyprland uses 'windowsIn' for window open
-      "windowsOut, 1, 1.5, easeInOut" # Hyprland uses 'windowsOut' for window close
-      "layers, 1, 2, easeInOut"
-    ];
+  renderCurve =
+    name: points: "hl.curve(${toLua name}, { type = \"bezier\", points = ${toLua points} })";
+
+  renderAnim = anim: "hl.animation(${toLua anim})";
+in
+{
+  options.hmModules.hyprland.animations = {
+    enable = lib.mkEnableOption "animations";
+
+    curves = lib.mkOption {
+      type = lib.types.attrsOf (lib.types.listOf lib.types.float);
+      default = { };
+    };
+
+    settings = lib.mkOption {
+      type = lib.types.listOf lib.types.attrs;
+      default = [ ];
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # Declarative definition:
+    hmModules.hyprland.animations = {
+      curves = {
+        easeInOut = [
+          0.4
+          0.0
+          0.2
+          1.0
+        ];
+      };
+      settings = [
+        {
+          leaf = "workspaces";
+          enabled = true;
+          speed = 1.5;
+          curve = "easeInOut";
+        }
+        {
+          leaf = "windows";
+          enabled = true;
+          speed = 2.0;
+          curve = "easeInOut";
+        }
+        {
+          leaf = "windowsIn";
+          enabled = true;
+          speed = 1.5;
+          curve = "easeInOut";
+        }
+        {
+          leaf = "windowsOut";
+          enabled = true;
+          speed = 1.5;
+          curve = "easeInOut";
+        }
+        {
+          leaf = "layers";
+          enabled = true;
+          speed = 2.0;
+          curve = "easeInOut";
+        }
+      ];
+    };
+
+    wayland.windowManager.hyprland.extraLuaFiles."animations" = ''
+      -- onStart: curves
+      ${lib.concatStringsSep "\n" (lib.mapAttrsToList renderCurve cfg.curves)}
+
+      -- onStart: animations
+      ${lib.concatMapStringsSep "\n" renderAnim cfg.settings}
+    '';
   };
 }
