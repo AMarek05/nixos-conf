a7855ff7078f5fbe25d143b2469b9db8d3ed8c9b
Adam Marek <amarek.git@gmail.com>
2026-08-28 18:37:20 +0200

    fixup! ghostty: set font to propo symbols

--- modules/hm/terminal/ghostty.nix
+++ modules/hm/terminal/ghostty.nix
@@ -17,6 +17,7 @@ in
         clipboard-read = "allow";
 
         font-family = "0xProto Nerd Font Propo";
+        adjust-cell-height = "-10%";
 
         app-notifications = false;
         shell-integration-features = "ssh-env,ssh-terminfo,cursor,sudo,title";
