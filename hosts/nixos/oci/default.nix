8211a7c14484a7bb2dedb30b1d5713113259222b
Adam Marek <amarek.git@gmail.com>
2026-09-07 14:16:30 +0000

    oci: add nh, fix network waiting

--- hosts/nixos/oci/default.nix
+++ hosts/nixos/oci/default.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./conf.nix
     ./pangolin.nix
     ./minecraft.nix
   ];
+
+  environment.systemPackages = with pkgs; [ nh ];
+  environment.sessionVariables = {
+    NH_FLAKE = "/home/adam/sys";
+  };
 }
