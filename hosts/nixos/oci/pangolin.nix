d1319f7f6f3f51e274e3ae97a1e99f6820ed551d
Claw <278452676+amarek-machine@users.noreply.github.com>
2026-09-07 16:38:35 +0200

    fix(oci): escape dollar signs in pangolin-env-init ExecStart
    systemd's ExecStart parser interprets $VARNAME as environment
    variable references, not shell variables. With unescaped $f, $s,
     systemd complained 'unset environment variable: f' and ran the
    script with empty values, writing the env file to the wrong path.
    
    Escape \$ so systemd passes literal $ to /bin/sh -c, where the
    shell resolves them as variables in the local scope.

--- hosts/nixos/oci/pangolin.nix
+++ hosts/nixos/oci/pangolin.nix
@@ -97,7 +97,7 @@
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStart = "/bin/sh -c ''f=/etc/nixos/secrets/pangolin.env; test -f $f || { umask 037; install -d -m 0755 /etc/nixos/secrets; s=$(head -c 32 /dev/urandom | base64 -w 0); echo SERVER_SECRET=$s > $f; chown pangolin:fossorial $f; chmod 0640 $f; }''";
+      ExecStart = "/bin/sh -c ''f=/etc/nixos/secrets/pangolin.env; test -f \$f || { umask 037; install -d -m 0755 /etc/nixos/secrets; s=\$(head -c 32 /dev/urandom | base64 -w 0); echo SERVER_SECRET=\$s > \$f; chown pangolin:fossorial \$f; chmod 0640 \$f; }''";
     };
   };
 
